e19c2a8356783232fdae549b78aadbaeed7a59b2
Viktor Ferenczi <viktor@ferenczi.eu>
2026-01-05 03:07:40 +0100

    Globe rotation

---
+++ Scenes/Editor/planet_view.gd
@@ -0,0 +1,28 @@
+extends SubViewportContainer
+class_name PlanetView
+
+@onready var viewport: SubViewport = %SubViewport
+@onready var planet: Planet = %Planet
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_planet_input_event_globe(lat: float, lon: float, event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		print("Click: lat=%+d, lon=%+d" % [roundi(lat), roundi(lon)])
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
+			planet.lat = lat
+			planet.lon = lon
+
+
+func _on_planet_input_event_map(lat: float, lon: float, event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		print("Click: lat=%+d, lon=%+d" % [roundi(lat), roundi(lon)])
--- Scenes/Planet/planet.gd
+++ Scenes/Planet/planet.gd
@@ -1,6 +1,8 @@
-@tool
 extends Node3D
 class_name Planet
+
+signal input_event_globe(lat: float, lon: float, event: InputEvent)
+signal input_event_map(lat: float, lon: float, event: InputEvent)
 
 @export var show_map: bool = false;
 @export_range(-90, 90, 1.0, "Latitude") var lat: float = 0.0;
@@ -18,3 +20,18 @@ func _process(_delta: float) -> void:
 	
 	if globe:
 		globe.rotation = Vector3(deg_to_rad(lat), deg_to_rad(180 - lon), deg_to_rad(angle))
+
+
+func _on_globe_physics_body_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	var local_pos = globe.transform.inverse() * event_position
+	var rad = Vector2(local_pos.x, local_pos.z).length()
+	var lat = rad_to_deg(atan2(local_pos.y, rad))
+	var lon = rad_to_deg(atan2(-local_pos.x, -local_pos.z))
+	input_event_globe.emit(lat, lon, event)
+
+
+func _on_map_physics_body_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	var local_pos = map.transform.inverse() * event_position
+	var lat = local_pos.y * 90
+	var lon = local_pos.x * 180
+	input_event_map.emit(lat, lon, event)
